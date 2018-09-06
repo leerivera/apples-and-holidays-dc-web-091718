c6fd69af1c14ddc093f1f24d079b7ace7cffb94e
leerivera80@gmail.com Rivera <leerivera80@gmail.com>
2018-09-06 01:47:08 +0000

    automatically backed up by learn

--- lib/holiday.rb
+++ lib/holiday.rb
@@ -53,7 +53,11 @@ def add_new_holiday_with_supplies(holiday_hash, season, holiday_name, supply_arr
 end
 
 def all_winter_holiday_supplies(holiday_hash)
-  # return an array of all of the supplies that are used in the winter season
+  # return an array of all of the supplies that are used in the
+  holiday_hash[:winter].collect do |holiday, supplies| 
+    supplies  
+  end.flatten 
+  winter season
 
 end
 
